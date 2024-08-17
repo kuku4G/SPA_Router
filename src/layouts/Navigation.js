601720eadfd117928d2bf5c6b3323b66b80ef1b7
Paulina Kukuczka <paula.create4you@gmail.com>
2024-08-17 13:31:45 +0200

    Create navigation and add basic styles in navigation.css

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/Navigation.css'
+
+const list = [
+    {name: "Start", path: "/"},
+    {name: "Produkty", path: "/products"},
+    {name: "Kontakt", path: "/contact"},
+    {name: "Panel admina", path: "/admin"},
+]
 
 const Navigation = () => {
-    return ( 
-        <div>
-            Nav
-        </div>
-     );
-}
- 
+
+    const menu = list.map(item => (
+        <li key={item.name}>
+            <NavLink to={item.path}>{item.name}</NavLink>
+        </li>
+    ))
+	return (
+			<nav className='main'>
+				<ul>
+					{menu}
+				</ul>
+			</nav>
+	);
+};
+
 export default Navigation;
