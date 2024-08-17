48877621d77a3697a430a1253ff2dfb5d9daef26
Paulina Kukuczka <paula.create4you@gmail.com>
2024-08-17 12:58:36 +0200

    Install react-router and create BASIC STRUCTURE in layoout

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,11 +1,26 @@
+import { React, Component } from 'react';
 import '../styles/App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
 
-function App() {
-  return (
-    <div className="App">
-SPA
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className='App'>
+					<header>{<Header />}</header>
+					<main>
+						<aside>{<Navigation />}</aside>
+						<section className='page'>{<Page />}</section>
+					</main>
+					<footer>{<Footer />}</footer>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
