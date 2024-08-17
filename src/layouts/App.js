27985b47d831db00225440d31467f8ab43b899f3
Paulina Kukuczka <paula.create4you@gmail.com>
2024-08-17 21:08:33 +0200

    Update. Create production version. Prepare for gh-pages

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,6 +1,6 @@
 import { React, Component } from 'react';
 import '../styles/App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
 import Navigation from './Navigation';
 import Page from './Page';
@@ -9,7 +9,7 @@ import Footer from './Footer';
 class App extends Component {
 	render() {
 		return (
-			<Router>
+			<Router basename={process.env.PUBLIC_URL}>
 				<div className='app'>
 					<header>{<Header />}</header>
 					<main>
