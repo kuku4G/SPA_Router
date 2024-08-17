b9c23898127e2ff2b33737167383bf50a8eef805
Paulina Kukuczka <paula.create4you@gmail.com>
2024-08-17 17:22:08 +0200

    Add image in section Header and add styles in Header.css

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import '../styles/Header.css';
+import header1 from '../images/header1.jpg';
+import header2 from '../images/header2.jpg';
+import header3 from '../images/header3.jpg';
 
 const Header = () => {
-	return <div>
-        Header
-    </div>;
+	return (
+		<div>
+			<Routes>
+				<Route path='/' element={<img src={header1} alt='city' />} />
+				<Route path='/products' element={<img src={header2} alt='city' />} />
+				<Route path='/contact' element={<img src={header3} alt='city' />} />
+				<Route path='/admin' element={<img src={header1} alt='city' />} />
+			</Routes>
+		</div>
+	);
 };
 
 export default Header;
