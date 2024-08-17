4ce458cbdda353911087de6480f5f3e9d3c1736a
Paulina Kukuczka <paula.create4you@gmail.com>
2024-08-17 19:54:27 +0200

    Create Contact panel with message sending supportand handling sending messages

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/ContactPage.css';
 
 const ContactPage = () => {
-    return ( 
-        <div>Kontakt</div>
-     );
-}
- 
+	const [value, setValue] = useState('');
+	const navigate = useNavigate();
+
+	const handleNavigation = () => {
+		if (value === '') {
+			const confirmed = window.confirm(
+				'You have empty message, write something'
+			);
+
+			if (!confirmed) {
+				return;
+			}
+		}
+
+		navigate('/contact');
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setValue('');
+		navigate('/contact');
+	};
+
+	const handleChange = (e) => {
+		setValue(e.target.value);
+	};
+
+	return (
+		<div className='contact'>
+			<form onSubmit={handleSubmit}>
+				<h3>Send message!</h3>
+				<textarea
+					placeholder='Your message'
+					value={value}
+					onChange={handleChange}
+				></textarea>
+				<button type='submit' onClick={handleNavigation}>
+					Send
+				</button>
+			</form>
+		</div>
+	);
+};
+
 export default ContactPage;
